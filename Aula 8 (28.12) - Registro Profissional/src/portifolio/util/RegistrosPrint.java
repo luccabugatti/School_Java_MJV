ff072ed7c5a49374f6000cb59a1693bf1c4df41c
luccabugatti <Lbugattilive@gmail.com>
2022-01-03 04:09:04 -0300

    Formatando Valores

--- Aula 8 (28.12) - Registro Profissional/src/portifolio/util/RegistrosPrint.java
+++ Aula 8 (28.12) - Registro Profissional/src/portifolio/util/RegistrosPrint.java
@@ -5,16 +5,17 @@ import portifolio.model.Registro;
 public class RegistrosPrint {
 	
 	public void imprimir(Registro registro) {
+		Formatter formatter = new Formatter();
 		StringBuilder fichaProfissional = new StringBuilder();
 		fichaProfissional.append("\n                  FICHA PROFISSIONAL");
 		fichaProfissional.append("\n\n PROFISSÃO: " + registro.getProfissao());
 		fichaProfissional.append("\n Nome: " + registro.getNomeCompleto());
 		fichaProfissional.append("\n Salário Minimo Requerido: Minimo R$ " + registro.getMinimoSalario() + "   Máximo R$ " + registro.getMaximoSalario());
-		fichaProfissional.append("\n Data de Nasc: " + registro.getDataNascimento() + " CPF: " + registro.getCpf() + " RG: " + registro.getRg());
+		fichaProfissional.append("\n Data de Nasc: " + formatter.data(registro.getDataNascimento()) + " CPF: " + formatter.cpf(registro.getCpf())  + " RG: " +  formatter.rg(registro.getRg()));
 		fichaProfissional.append("\n Endereço: " + registro.getLogradouro());
 		fichaProfissional.append("\n N°: " + registro.getNumeroCasa() + "    Bairro: " + registro.getBairro() + "    Municipio: " + registro.getCidade() + "    UF: " + registro.getUf());
-		fichaProfissional.append("\n Cep: " + registro.getCep() + "    Naturalidade: " + registro.getNaturalidade() + "    País: " + registro.getPais());
-		fichaProfissional.append("\n Telefone: " + registro.getTelefone() + "     Celular: " + registro.getCelular());
+		fichaProfissional.append("\n Cep: " + formatter.cep(registro.getCep()) + "    Naturalidade: " + registro.getNaturalidade() + "    País: " + registro.getPais());
+		fichaProfissional.append("\n Telefone: " + formatter.formataTelefone(registro.getTelefone()) + "     Celular: " + formatter.formataTelefone(registro.getCelular()));
 		fichaProfissional.append("\n Email: " + registro.getEmail());		
 		fichaProfissional.append("\n\n ------------------------------------------------------------- \n\n");
 		

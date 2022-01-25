60036e02c68360a301da1272daaacbdd4dc84f24
luccabugatti <Lbugattilive@gmail.com>
2022-01-25 19:29:12 -0300

    Recriando projeto e adicionando template

--- Projeto final/easyjob/src/main/java/mjv/easyjob/EasyjobApplication.java
+++ Projeto final/easyjob/src/main/java/mjv/easyjob/EasyjobApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EasyJobApplication {
+public class EasyjobApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EasyJobApplication.class, args);
+		SpringApplication.run(EasyjobApplication.class, args);
 	}
 
 }
--- Projeto final/easyjob/src/test/java/mjv/easyjob/EasyjobApplicationTests.java
+++ Projeto final/easyjob/src/test/java/mjv/easyjob/EasyjobApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EasyJobApplicationTests {
+class EasyjobApplicationTests {
 
 	@Test
 	void contextLoads() {
